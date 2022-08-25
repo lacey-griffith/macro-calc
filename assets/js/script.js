4ebf6a881c4f7ce9aaa489c4b6f05cd7e82941d3
Lacey <lacey.griffith04@gmail.com>
2022-08-24 20:07:56 -0500

    adding fields

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,71 +1,20 @@
 function createCalcs(){
-    let calculater = $('.calculater-body');
-    if(calculater.length == 0){
-        setTimeout(createCalcs, 50);
-        return;
-    }
-
-    //gather value events
-    $('#current_weight input').on('change blur',function(){
-        let current_weight = $(this).val();
-    });
-
-    $('#goal_weight input').on('change blur',function(){
-        let goal_weight = $(this).val();
-    });
-
-    $('#activity span').on('click',function(){
+    $('.calc-item span.button').on('click', function(){
         if($(this).hasClass('active')){
             return;
         } else {
-            $('#activity span.active').removeClass('active');
-            $(this).addClass('active')
+            $(this).parent().find('span.button.active').removeClass('active');
+            $(this).addClass('active');
         }
     });
 
-    $('.calc-cta').on('click', function(){
-        if($('#activity span.active').length == 0){
-            $('#activity .error').show();
-        } else {
-            $('#activity .error').hide();
+    $('.calc-item span.button.height').on('click', function(){
+        let trigger = $(this).attr('data-trigger');
+        if($(`div.height_option[data-trigger="${trigger}"]`).hasClass('active')){
+           return; 
         }
-
-        if($('#current_weight input').val().length == 0){
-            $('#current_weight .error').show();
-        } else {
-            $('#current_weight .error').hide();
-        }
-
-        if($('#goal_weight input').val().length == 0){
-            $('#goal_weight .error').show();
-        } else {
-            $('#goal_weight .error').hide();
-        }
-
-        if(!$('#activity span.active').length == 0 && !$('#current_weight input').val().length == 0 && !$('#goal_weight input').val().length == 0){
-            let current = Number($('#current_weight input').val());
-            let activity = Number($('#activity span.active').attr('value'));
-
-            let maint_cal = current * activity;
-            maint_cal = maint_cal - 500;
-            let protein = current * 0.8;
-            let fat = current * 0.3;
-
-            let protein_g = protein *4;
-            let fat_g = fat*9;
-
-            let x = protein_g + fat_g;
-            let carb_cal = maint_cal - x;
-
-            carbs = (carb_cal/4);
-
-            $('.results_body .cal_result').html(`${Math.round(maint_cal)} calories`);
-            $('.results_body .protein_result').html(`${Math.round(protein)} grams`);
-            $('.results_body .fat_result').html(`${Math.round(fat)} grams`);
-            $('.results_body .carb_result').html(`${Math.round(carbs)} grams`);
-            $('.results_body').addClass('show');
-        }
-
+        $(`div.height_option.active`).removeClass('active');
+        $(`div.height_option[data-trigger="${trigger}"]`).addClass('active');
     });
 }
 
