184fa219c88c4d4de5cacde860e0636e2c4b791b
Lacey <lacey.griffith04@gmail.com>
2022-08-29 21:06:47 -0500

    adding some calc logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 function createCalcs(){
+
     $('.calc-item span.button').on('click', function(){
         if($(this).hasClass('active')){
             return;
@@ -15,6 +16,25 @@ function createCalcs(){
         }
         $(`div.height_option.active`).removeClass('active');
         $(`div.height_option[data-trigger="${trigger}"]`).addClass('active');
+    });
+
+    $('.calc-cta').on('click', function(){
+        let g = $('#gender .active').attr('value');
+        let w = $('#current_weight').attr('value');
+
+        if(g == 'male'){
+         //Men: calories/day = 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5   
+        }
+        if(g == 'female'){
+        //Women: calories/day = 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) – 161
+        }
+
+        if(w === 'lbs'){
+            //convert to kg
+        }
+        if(w === 'kg'){
+            //is kilograms
+        }
     });
 }
 
