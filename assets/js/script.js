c064b6a7349a43a0e9a49f274b0773b2ebf8618d
Lacey <lacey.griffith04@gmail.com>
2022-09-03 19:45:34 -0500

    add calc functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 function createCalcs(){
 
+    //change active selection bubbles
     $('.calc-item span.button').on('click', function(){
         if($(this).hasClass('active')){
             return;
@@ -9,6 +10,7 @@ function createCalcs(){
         }
     });
 
+    //change active inputs for HEIGHT option(FT+IN VS CM)
     $('.calc-item span.button.height').on('click', function(){
         let trigger = $(this).attr('data-trigger');
         if($(`div.height_option[data-trigger="${trigger}"]`).hasClass('active')){
@@ -19,24 +21,89 @@ function createCalcs(){
     });
 
     $('.calc-cta').on('click', function(){
-        let g = $('#gender .active').attr('value');
-        let w = $('#current_weight').attr('value');
-
-        if(g == 'male'){
-         //Men: calories/day = 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) + 5   
-        }
-        if(g == 'female'){
-        //Women: calories/day = 10 x weight (kg) + 6.25 x height (cm) – 5 x age (y) – 161
-        }
-
-        if(w === 'lbs'){
-            //convert to kg
-        }
-        if(w === 'kg'){
-            //is kilograms
-        }
+        //check for errors and then calculate
+        doMaths();
     });
 }
 
+function doMaths(){  
+    details = [];
+    $('.calc-item span.active').each(function(){
+        let value = $(this).attr('data-trigger');
+        let label = $(this).parent().attr('id');
 
-    createCalcs()
+        let item = {label: `${label}`, value: `${value}`};
+        details.push(item);
+        //console.log(details)
+    });
+
+    //if weight is in lbs --> convert to kg
+    let w;
+    let weight = details.filter(x => x.label === 'current_weight');
+    let isKg = weight[0].value.indexOf('kg') > -1;
+    if(isKg){
+        w = $('#current_weight input').val();
+    } else {
+        w = $('#current_weight input').val()*0.453592;
+    }
+
+    //if height is in ft/in --> convert to cm 
+    let h;
+    let height = details.filter(x => x.label === 'current_height');
+    let isCm = height[0].value.indexOf('cm') > -1;
+    if(isCm){
+        h = Number($('#current_height .active input').val());
+    } else {
+        let ft = $('#current_height input[placeholder="ft"]').val()*12;
+        let inch = $('#current_height input[placeholder="in"]').val();
+        h = (Number(ft) + Number(inch))*2.54; 
+    }
+
+    //age
+    let y;
+    y = Number($('#age input').val());
+
+    //gender
+    //let gender = details.filter(x => x.label === 'gender');
+    let result = (10*w) + (6.25*h) - (5*y)
+    let trigger = $('#gender .button.active').attr('data-trigger');
+    if(trigger === 'female'){
+        result = result - 161;
+    }
+
+    if(trigger === 'male'){
+        result = result + 5;
+    }
+
+    //activity level
+    let activity = Number($('#activity .button.activity-level.active').attr('value'));
+    let TDEE = result*activity
+
+    let daily_cal = TDEE-500;
+    $('.results_body .cal_result').text(daily_cal.toFixed());
+
+    //protein 35% 4 calories per gram
+    let protein = (daily_cal*0.35)/4;
+    $('.results_body .protein_result').text(`${protein.toFixed()}g`)
+
+    //fat 30% 9 calories per gram
+    let fat = (daily_cal*0.3)/9;
+    $('.results_body .fat_result').text(`${fat.toFixed()}g`);
+
+    //carb 35% 4 calories per gram
+    let carb = (daily_cal*0.35)/4;
+    $('.results_body .carb_result').text(`${carb.toFixed()}g`);
+
+    $('.results_body').addClass('show');
+}
+/*
+            <div class="results_body">
+                <div>daily calorie goal: <span class="cal_result"></span></div>
+                <div>protein: <span class="protein_result"></span></div>
+                <div>fat: <span class="fat_result"></span></div>
+                <div>carbs: <span class="carb_result"></span></div>
+            </div>
+*/
+
+    createCalcs();
+    //doMaths();
